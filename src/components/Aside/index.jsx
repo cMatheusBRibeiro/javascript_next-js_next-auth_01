896a30f70c0878e14498f94b2aab194300489eea
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-01-13 20:29:59 -0300

    feat(MenuLateral): Criado menu lateral

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,14 +1,52 @@
-import Image from 'next/image'
-import styles from './aside.module.css'
+import Image from "next/image";
+import styles from "./aside.module.css";
 
-import logo from './logo.png'
-import Link from 'next/link'
+import logo from "./logo.png";
+import Link from "next/link";
+import AsideLink from "../AsideLink";
+import { Feed } from "../icons/Feed";
+import { Account } from "../icons/Account";
+import { Info } from "../icons/Info";
+import { Login } from "../icons/Login";
+import { Button } from "../Button";
 
 export const Aside = () => {
-    return (<aside className={styles.aside}>
-        {/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
-        <Link href="/">
-            <Image src={logo} alt="Logo da Code Connect"/>
-        </Link>
-    </aside>)
-}
+    return (
+        <aside className={styles.aside}>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">
+                            <Image src={logo} alt="Logo da Code Connect" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Button href="/publicar" outline>
+                            Publicar
+                        </Button>
+                    </li>
+                    <li>
+                        <AsideLink href="/feed">
+                            <Feed /> Feed
+                        </AsideLink>
+                    </li>
+                    <li>
+                        <AsideLink href="/perfil">
+                            <Account /> Perfil
+                        </AsideLink>
+                    </li>
+                    <li>
+                        <AsideLink href="/sobre-nos">
+                            <Info /> Sobre nós
+                        </AsideLink>
+                    </li>
+                    <li>
+                        <AsideLink href="/logout">
+                            <Login /> Sair
+                        </AsideLink>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    );
+};
